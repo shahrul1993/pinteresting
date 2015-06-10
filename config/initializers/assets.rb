85878d11cd07476de92bc45376b8c17760866d49
Shahrul Hamdan <shahrul.sufian@gmail.com>
2015-06-10 14:18:45 +0100

    Navbar Not Working

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
+Rails.application.config.assets.precompile += %w( glyphicons-halflings.png )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-white.png )
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
